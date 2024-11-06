39abe2c276c43f86a69cf2a6867b7a4333b8ebbf
YachWise <jacobhwiseios@gmail.com>
2024-11-06 01:53:22 -0600

    November 6th Question Setup

---
+++ 3011_Nov_6_2024/3011_Nov_6_2024/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] arrTester = new Array[];
+        CanSortArray(arrTester);
+    }
+    public static bool CanSortArray(int[] nums)
+    {
+        return false;
+    }
+
+
+}
